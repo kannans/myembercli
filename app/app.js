9df96c65778eff881671434396b49f267f31cc56
kannanhudson <kannanhudson@gmail.com>
2014-09-24 16:31:58 +0530

    Initial commit

--- app/app.js
+++ app/app.js
@@ -3,6 +3,9 @@ import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
+import trimHelper from './helpers/trim';
+Ember.Handlebars.registerBoundHelper('trim', trimHelper);
+
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
